abd31acfee4c3d52740e763b1568ea5ec8c4df86
Janis Basis Basovs <janis_basis@hotmail.com>
2021-01-24 20:40:20 +0200

    Add inital scale 1

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,16 @@
 import styled from "styled-components"
+import Head from "next/head"
 
 const Layout = ({ children }) => {
   return (
     <MyComponent>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </Head>
+
       <main>{children}</main>
     </MyComponent>
   )
